767dbb93ac59507f881ae026011ad8d9371b2d41
papande <danielsiagian1986@gmail.com>
2026-06-28 00:13:26 +1000

    Obfuscate keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// REPLACE THIS OBJECT WITH YOUR ACTUAL KEYS FROM FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyBn-Kv71HcuHqz0W0mm2EeDRZoTh-QhnC8",
-  authDomain: "spelling-bee-app-55a71.firebaseapp.com",
-  databaseURL: "https://spelling-bee-app-55a71-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "spelling-bee-app-55a71",
-  storageBucket: "spelling-bee-app-55a71.firebasestorage.app",
-  messagingSenderId: "1016413492434",
-  appId: "1:1016413492434:web:5d1b49347d48d4773f890b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
 export const db = getDatabase(app);
